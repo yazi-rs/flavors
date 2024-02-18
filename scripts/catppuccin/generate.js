b54b5149de535f7c31a20bdfd42c27a5de5cd44f
sxyazi <sxyazi@gmail.com>
2024-02-18 21:09:42 +0800

    feat: add new color scheme for catppuccin

--- scripts/catppuccin/generate.js
+++ scripts/catppuccin/generate.js
@@ -1,23 +1,20 @@
-import { variants, labels as colors } from "@catppuccin/palette";
-import { readFile, writeFile } from "node:fs/promises";
-import { join } from "node:path";
-import { fileURLToPath } from "url";
+import { variants, labels as colors } from "@catppuccin/palette"
+import { readFile, writeFile } from "node:fs/promises"
+import { join } from "node:path"
+import { fileURLToPath } from "node:url"
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const __dirname = fileURLToPath(new URL(".", import.meta.url))
 
-const template = await readFile(join(__dirname, "./template.toml"), "utf-8");
+const template = await readFile(join(__dirname, "./template.toml"), "utf8")
 
 for (const variant of Object.keys(variants)) {
-	let theme = template;
+	let theme = template
 
 	for (const [name, color] of Object.entries(colors)) {
-		theme = theme.replace(new RegExp(`{{ ${name} }}`, "g"), color[variant].hex);
+		theme = theme.replaceAll(new RegExp(`{{ ${name} }}`, "g"), color[variant].hex)
 	}
 
-	theme = theme.replace(new RegExp("{{ variant }}", "g"), variant);
+	theme = theme.replaceAll(new RegExp("{{ variant }}", "g"), variant)
 
-	await writeFile(
-		join(__dirname, "../../catppuccin/", variant + ".toml"),
-		theme
-	);
+	await writeFile(join(__dirname, `../../catppuccin-${variant}/`, "theme.toml"), theme)
 }
