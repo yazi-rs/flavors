8f8baa090ecce2ae65464238f153c9f3067f5acb
sxyazi <sxyazi@gmail.com>
2024-04-07 23:58:10 +0800

    feat: update generation script

--- scripts/catppuccin/generate.js
+++ scripts/catppuccin/generate.js
@@ -6,15 +6,22 @@ import { fileURLToPath } from "node:url"
 const __dirname = fileURLToPath(new URL(".", import.meta.url))
 
 const template = await readFile(join(__dirname, "./template.toml"), "utf8")
+const readme = await readFile(join(__dirname, "./README.md"), "utf8")
 
 for (const variant of Object.keys(variants)) {
-	let theme = template
-
+	// flavor.toml
+	let flavor = template
 	for (const [name, color] of Object.entries(colors)) {
-		theme = theme.replaceAll(new RegExp(`{{ ${name} }}`, "g"), color[variant].hex)
+		flavor = flavor.replaceAll(`{{ ${name} }}`, color[variant].hex)
 	}
+	flavor = flavor.replaceAll("{{ variant }}", variant)
+	await writeFile(join(__dirname, `../../catppuccin-${variant}.yazi/flavor.toml`), flavor)
 
-	theme = theme.replaceAll(new RegExp("{{ variant }}", "g"), variant)
-
-	await writeFile(join(__dirname, `../../catppuccin-${variant}/`, "theme.toml"), theme)
+	// README.md
+	await writeFile(
+		join(__dirname, `../../catppuccin-${variant}.yazi/README.md`),
+		readme
+			.replaceAll("{{ variant }}", variant)
+			.replaceAll("{{ variant_cap }}", variant.charAt(0).toUpperCase() + variant.slice(1)),
+	)
 }
